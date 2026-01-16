e11e8952b1d121225169ff57fdcdb004bed03c0a
Appbird <project.appbird@gmail.com>
2026-01-16 16:57:13 +0900

    PDFLayoutに関する型と巡回法のサンプルコード実装

---
+++ src/pdf2text.py
@@ -0,0 +1,51 @@
+import json
+from pathlib import Path
+from returns.result import safe
+import pymupdf.layout as _
+import pymupdf4llm
+import argparse
+
+from pymupdf_layout_types import PdfDocument, list_span_texts
+
+@safe
+def pdf2txt(
+    path_pdf:Path,
+    cached:bool = True
+) -> str:
+    """`path_pdf`に与えられたPDFをpymupdf4llmによってテキスト化する。"""
+    path_txt = path_pdf.with_name(f"{path_pdf.name}.txt")
+    if path_txt.exists() and cached:
+        return path_txt.read_text(encoding="utf-8")
+    txt = pymupdf4llm.to_markdown(path_pdf)
+    if not isinstance(txt, str):
+        raise Exception("pymupdf4llm exported list[dict].")
+    if cached:
+        path_txt.write_text(txt, encoding="utf-8")
+    return txt
+
+@safe
+def pdf2json(
+    path_pdf:Path,
+    cached:bool = True
+) -> PdfDocument:
+    """`path_pdf`に与えられたPDFをpymupdf4llmによってJSON化する。"""
+    path_json = path_pdf.with_name(f"{path_pdf.name}.json")
+    if path_json.exists() and cached:
+        return PdfDocument.model_validate_json(path_json.read_text(encoding="utf-8"))
+    txt = pymupdf4llm.to_json(path_pdf, )
+    ob = json.loads(txt)
+    txt = json.dumps(ob, ensure_ascii=False)
+    path_json.write_text(txt, encoding="utf-8")
+    return PdfDocument.model_validate_json(txt)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='pdf2text',
+        description='convert pdf to text, json.',
+    )
+    parser.add_argument("filename", type=Path)
+    args= parser.parse_args()
+    pdf_document = pdf2json(args.filename).unwrap()
+    list_span_texts(pdf_document)
+
+    
---
+++ src/pymupdf_layout_types.py
@@ -0,0 +1,102 @@
+from typing import Any, Optional
+from pydantic import BaseModel
+
+type Bbox = list[float]
+type TocEntry = list[int | str]
+
+class Span(BaseModel):
+    size: float
+    flags: int
+    bidi: int
+    char_flags: int
+    font: str
+    color: int
+    alpha: int
+    ascender: float
+    descender: float
+    text: str
+    origin: list[float]
+    bbox: list[float]
+    #line: Optional[int]
+    #block: Optional[int]
+    #dir: Optional[list[float]]
+
+class TextLine(BaseModel):
+    bbox: Bbox
+    spans: list[Span]
+
+class Box(BaseModel):
+    x0: float
+    y0: float
+    x1: float
+    y1: float
+    boxclass: str
+    image: Optional[Any]
+    table: Optional[Any]
+    textlines: Optional[list[TextLine]]
+
+class FullTextLine(BaseModel):
+    spans: list[Span]
+    wmode: int
+    dir: list[float]
+    bbox: Bbox
+
+class FullTextBlock(BaseModel):
+    type: int
+    number: int
+    flags: int
+    bbox: Bbox
+    lines: list[FullTextLine]
+
+class Page(BaseModel):
+    page_number: int
+    width: float
+    height: float
+    boxes: list[Box]
+    full_ocred: bool
+    text_ocred: bool
+    fulltext: list[FullTextBlock]
+    words: list[Any]
+    links: list[Any]
+
+class Metadata(BaseModel):
+    format: str
+    title: str
+    author: str
+    subject: str
+    keywords: str
+    creator: str
+    producer: str
+    creationDate: str
+    modDate: str
+    trapped: str
+    encryption: Optional[Any]
+
+class PdfDocument(BaseModel):
+    filename: str
+    page_count: int
+    toc: list[TocEntry]
+    pages: list[Page]
+    metadata: Metadata
+    from_bytes: bool
+    image_dpi: int
+    image_format: str
+    image_path: str
+    use_ocr: bool
+    form_fields: dict[str, Any]
+    force_text: bool
+    embed_images: bool
+    write_images: bool
+
+
+def list_span_texts(doc: PdfDocument) -> list[str]:
+    texts: list[str] = []
+    for page in doc.pages:
+        print(f"Page {page.page_number};")
+        for box in page.boxes:
+            print(f"\tbox {box.boxclass}")
+            if box.textlines is None: continue
+            for textline in box.textlines:
+                for span in textline.spans:
+                    print(f"\t\t{span.text}")
+    return texts
